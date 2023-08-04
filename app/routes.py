e5e9f03e3d03af6a56bce558ac5e85a71d733356
actirov <emiliano.mayen1707@gmail.com>
2023-08-03 18:37:09 -0600

    comment

--- app/routes.py
+++ app/routes.py
@@ -7,10 +7,9 @@ def named_entity_recognition():
     data = request.get_json() # Obtener los datos JSON enviados con la solicitud.
     sentences = data.get("oraciones", []) # Extrae las oraciones de los datos.
 
-	# Compruebe si las oraciones se proporcionan correctamente.
+    # Compruebe si las oraciones se proporcionan correctamente.
     if not sentences or not isinstance(sentences, list):
-        return jsonify({"error": "The 'oraciones' field must be a list of sentences."}), 400 # Devuelve un error si las oraciones no se 
-proporcionan correctamente.
+        return jsonify({"error": "The 'oraciones' field must be a list of sentences."}), 400 # Devuelve un error si las oraciones no se proporcionan correctamente.
     
     entities = recognize_entities(sentences) # Obtener las entidades reconocidas para cada oración.
 
--- run.py
+++ run.py
@@ -2,5 +2,4 @@ from app import app
 
 # La aplicación se ejecuta desde este archivo. Importa la instancia de Flask y la ejecuta.
 if __name__ == "__main__":
-    app.run(debug=True)  #debug=True habilita el modo de debugging, proporcionando mensajes de error detallados en el navegador cuando algo 
-sale mal.
+    app.run(debug=True)  #debug=True habilita el modo de debugging, proporcionando mensajes de error detallados en el navegador cuando algo sale mal.
